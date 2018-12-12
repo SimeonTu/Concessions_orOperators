e72733ee893c22ca5b3e573460669575271ea5e9
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2018-12-12 04:19:56 +0000

    Add files via upload

---
+++ Concessions_orOperators.py
@@ -0,0 +1,26 @@
+
+
+name = input("What is your name? ")
+
+student_str = input("Are you a student [y/n]? ")
+student = student_str.lower() in ("y", "yes")
+
+age = int(input("How old are you? "))
+
+message = "Hello " + name
+
+def get_message(student,age,message):
+    if student and (age <= 18 or age > 65):
+        message += " - congratulations, you are entitled to a 20% discount"
+    elif student or (age <= 18 or age > 65):
+        message += " - congratulations, you are entitled to a 10% discount"
+    else:
+        message += " - sorry, you must pay the regular price"
+    print(message)
+
+get_message(student,age,message)
+
+print(message)
+
+print()
+input("Press return to continue ...")
